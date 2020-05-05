972eb0e1f861fb4dd94d69492e7388858d6e9d29
Jefferson Mendes <jefferson.mendes.santos@gmail.com>
2020-05-05 00:49:43 -0300

    adicionando testes unitários

--- src/test/java/com/bora/ProfileRepositoryTests.java
+++ src/test/java/com/bora/ProfileRepositoryTests.java
@@ -19,7 +19,7 @@ public class ProfileRepositoryTests {
 	@Test
 	public void shouldCreateProfile() {
 		Profile profile = new Profile("Email teste", "Nome teste", true, true, true, false, false, false);
-		this.userRepository.save(profile);
+		userRepository.save(profile);
 		Assertions.assertThat(profile.getEmail()).isNotNull();
 		Assertions.assertThat(profile.getNome()).isNotNull();
 		Assertions.assertThat(profile.getSolteiro()).isTrue();
@@ -28,5 +28,37 @@ public class ProfileRepositoryTests {
 		Assertions.assertThat(profile.getSairAnoite()).isFalse();
 		Assertions.assertThat(profile.getDescanso()).isFalse();
 		Assertions.assertThat(profile.getAventureiro()).isFalse();
+	}
+	
+	@Test
+	public void shouldUpdateProfile() {
+		Profile profile = new Profile("Email teste", "Nome teste", true, true, true, false, false, false);
+		userRepository.save(profile);
+		
+		profile.setNome("Novo nome");
+		profile.setSolteiro(false);
+		profile.setAventureiro(false);
+		profile.setDescanso(true);
+		profile.setFilho(false);
+		profile.setPet(false);
+		profile.setSairAnoite(true);
+		userRepository.save(profile);
+		
+		profile = userRepository.findById(profile.getEmail()).get();
+		Assertions.assertThat(profile.getNome()).isEqualTo("Novo nome");
+		Assertions.assertThat(profile.getSolteiro()).isEqualTo(false);
+		Assertions.assertThat(profile.getAventureiro()).isEqualTo(false);
+		Assertions.assertThat(profile.getDescanso()).isEqualTo(true);
+		Assertions.assertThat(profile.getFilho()).isEqualTo(false);
+		Assertions.assertThat(profile.getPet()).isEqualTo(false);
+		Assertions.assertThat(profile.getSairAnoite()).isEqualTo(true);
+	}
+	
+	@Test
+	public void shouldDeleteProfile() {
+		Profile profile = new Profile("Email teste", "Nome teste", true, true, true, false, false, false);
+		userRepository.save(profile);
+		userRepository.delete(profile);
+		Assertions.assertThat(userRepository.findById(profile.getEmail())).isEmpty();
 	}
 }
